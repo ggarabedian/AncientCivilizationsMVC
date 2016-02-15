4640417b65e43a3973c43ffabc88e202e042fc7a
ggarabedian <garabedian2001@gmail.com>
2016-02-15 19:53:48 +0200

    Re-factored the kendo grid helper

--- AncientCivilizations/Web/AncientCivilizations.Web.Common/Constants/Actions.cs
+++ AncientCivilizations/Web/AncientCivilizations.Web.Common/Constants/Actions.cs
@@ -3,5 +3,10 @@
     public class Actions
     {
         public const string Index = "Index";
+        public const string Read = "Read";
+        public const string Create = "Create";
+        public const string Update = "Update";
+        public const string Destroy = "Destroy";
+
     }
 }
--- AncientCivilizations/Web/AncientCivilizations.Web.Common/HtmlHelperExtensions/KendoHtmlHelpers.cs
+++ AncientCivilizations/Web/AncientCivilizations.Web.Common/HtmlHelperExtensions/KendoHtmlHelpers.cs
@@ -1,13 +1,21 @@
 ﻿namespace AncientCivilizations.Web.Common.HtmlHelperExtensions
 {
+    using System;
     using System.Web.Mvc;
 
     using Kendo.Mvc.UI;
     using Kendo.Mvc.UI.Fluent;
 
+    using Constants;
+
     public static class KendoHtmlHelpers
     {
-        public static GridBuilder<T> CustomGrid<T>(this HtmlHelper helper, string name)
+        public static GridBuilder<T> CustomGrid<T>(this HtmlHelper helper,
+            string name,
+            string controllerName,
+            bool canCreate,
+            Action<DataSourceModelDescriptorFactory<T>> factory,
+            bool rebindOnEdit = false)
            where T : class
         {
             return helper.Kendo().Grid<T>()
@@ -15,7 +23,18 @@
                 .Pageable(p => p.Refresh(true))
                 .Sortable()
                 .Filterable()
-                .ColumnMenu();
+                .ColumnMenu()
+                .ToolBar(toolbar => { if (canCreate) { toolbar.Create(); } })
+                .Editable(edit => edit.Mode(GridEditMode.PopUp))
+                .DataSource(dataSource => dataSource
+                    .Ajax()
+                    .PageSize(10)
+                    .Model(factory)
+                    .Read(read => read.Action(Actions.Read, controllerName))
+                    .Create(create => create.Action(Actions.Create, controllerName))
+                    .Update(update => update.Action(Actions.Update, controllerName))
+                    .Destroy(destroy => destroy.Action(Actions.Destroy, controllerName))
+                    .Events(events => { if (rebindOnEdit) { events.RequestEnd("rebindOnEdit"); } }));
         }
     }
 }
